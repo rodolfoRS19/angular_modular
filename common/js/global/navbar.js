19e18d73f0d58bf03a48662e90e4f2633aeef8d3
Rodolfo Rojas <rodolfo.rojas@prodigious.com>
2015-01-12 08:48:09 -0600

    navbar transition effect

--- common/js/global/navbar.js
+++ common/js/global/navbar.js
@@ -1,9 +1,11 @@
 define(['jquery'],function($){
 		$(window).scroll(function(){
-			$('nav').fadeOut("slow",function(){
+			if($(window).scrollTop() > 0 ){
+				$('nav').fadeOut("slow",function(){
 				$('nav').fadeIn("slow",function(){
 					return null;
 				});
 			});
+			}
 		});
 });
