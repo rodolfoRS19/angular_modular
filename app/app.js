4e837fd1ab071714fefca19e1b90035d8bb39637
Rodolfo Rojas <rodolfo.rojas@prodigious.com>
2015-01-14 10:02:59 -0600

    add router to app module

--- app/app.js
+++ app/app.js
@@ -4,5 +4,18 @@ define([
     './libs/common/js/home/loader'
 ], function (ng,$stateProvider) {
     'use strict';
-     return ng.module('app', ['ui.router'])
+     ng.module('app', ['ui.router'])
+
+     .config(function($stateProvider){
+
+	 		$stateProvider.state('contacts', {
+	 		  url:'/contacts',
+			  templateUrl: 'app/hello_module/partials/list_persons.html',
+			  controller:'personController'
+			})
+            $stateProvider.state('contacts.details', {
+                url:'/:id',
+                templateUrl:'app/hello_module/partials/details_contact.html'
+            });
+     	}
 });
