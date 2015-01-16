93b4a842cf36578ed4367ade7a73329a15255eec
Rodolfo Rojas <rodolfo.rojas@prodigious.com>
2015-01-16 12:38:43 -0600

    cssjoin grunt copy assets grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt){
 
 		//compile and minify the app js files and put into /build/prod/app/js/ dir.
 		requirejs:{
-			compile:{
+			compile_prod:{
 				options:{
 					baseUrl:".",
     				mainConfigFile:"./app/main.js",
@@ -17,6 +17,15 @@ module.exports = function(grunt){
               			mangle: false
           			}
 				}
+			},
+			compile_dev:{
+				options:{
+					baseUrl:".",
+    				mainConfigFile:"./app/main.js",
+    				name: './app/main',
+    				out: "./build/dev/app/js/app.js",
+    				optimize: "none",
+				}
 			}
 		},
 
@@ -25,16 +34,44 @@ module.exports = function(grunt){
   			prod: ["./build/prod/app"],
   			dev: ["./build/dev/app"]
 		},
+
+		copy:{
+			//copy requirejs file, the app depends of this file to run with sucess the first load 
+			require_prod:{
+				src: './app/libs/vendor/js/require_plugins/require.min.js',
+    			dest: './build/prod/app/js/require.min.js',
+			},
+			require_dev:{
+				src: './app/libs/vendor/js/require_plugins/require.js',
+				dest:'./build/dev/app/js/require.js'
+			},
+			//copy all asset dir to dev or prod dir, base on which build task is running 
+			assets_dev:{
+				file:[
+					{
+						expand: true, 
+						src: ['./app/assets/**'], 
+						dest: './build/dev/app/assets/'
+					}
+				]
+			},
+			assets_prod:{
+				file:[
+					{
+						expand: true, 
+						src: ['./app/assets/**'], 
+						dest: './build/prod/app/assets/'
+					}
+				]
+			}
+		},
 		//join the app css files and put into build/prod/app/css/ dir. this task must be run just to buil the app to development or test enviroment
 		cssjoin: {
-		    dev :{
-		      options: {
-		        paths : ["app/libs/"]
-		      },
-		      files: {
-		        'build/prod/app/css/app.min.css': ['app/libs/common/css/global/loader.css'],
-		      },
-    		}
+		      dev :{
+			      files: {
+			        'build/dev/app/css/global/app.loader.css': ['app/libs/common/css/app.loader.css']
+			      }
+    		  }
   		},
 
   		//css minification task must be run only to buil_prod task.
@@ -57,8 +94,11 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 	grunt.loadNpmTasks('grunt-cssjoin');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 
 	//Register Task
-	grunt.registerTask('prod_build',['clean:prod','requirejs:compile','cssmin:prod']);
+	grunt.registerTask('prod_build',['clean:prod','requirejs:compile_prod','cssmin:prod','copy:assets_prod','copy:require_prod']);
+
+	grunt.registerTask('dev_build',['clean:dev','requirejs:compile_dev','cssjoin:dev','copy:assets_dev','copy:require_dev']);
 }
